22024fd2749aa43bdf821f9329b31a518614704a
damowi02 <damowi02@hs-esslingen.de>
2019-12-24 10:05:06 +0100

    changes 24.12.19

--- tavascan.js
+++ tavascan.js
@@ -88,7 +88,7 @@ function HomeFunction() {
         
         
         
-        if(i === 0){ DataDiv.innerHTML = "Speed";}
+        if(i === 0){ DataDiv.innerHTML = "Speed: " + " "  ;}
         else if( i===1){DataDiv.innerHTML ="Gas";}
         else if( i===2){DataDiv.innerHTML ="Humidity";}
         else if( i===3){DataDiv.innerHTML ="Temperature";}
@@ -98,6 +98,57 @@ function HomeFunction() {
         mainElement.appendChild(DataDiv);
     }
 }
+
+document.getElementById("CarFunctionButton").addEventListener("click",handleClicks);
+
+ function ControlCarFunctions(){
+
+    let mainElement = document.querySelector("main");
+
+    mainElement.innerHTML = "";
+
+    let LockDiv = document.createElement("div");
+    LockDiv.classList.add("CarLockUnlockFunction");
+    mainElement.appendChild(LockDiv);
+
+    //LockDiv.setAttribute("style","display:inline;width:500px");
+
+    let WindowDiv = document.createElement("div");
+    WindowDiv.classList.add("WindowFunction");
+    mainElement.appendChild(WindowDiv);
+
+
+    for (let i=0; i<2; i++){
+
+        let UnlockLockDiv = document.createElement("div");
+
+        UnlockLockDiv.classList.add("CarLockUnlock");
+        UnlockLockDiv.style.display ="inline";
+        
+        
+        
+        if(i === 0){ UnlockLockDiv.innerHTML = "Lock";
+                    UnlockLockDiv.style.float = "left";
+                    UnlockLockDiv.style.marginLeft = "25%";
+                    UnlockLockDiv.style.fontSize = "2em";
+                    UnlockLockDiv.style.marginTop = "10%";
+                    UnlockLockDiv.id="Lock";
+        }
+        else if( i===1){UnlockLockDiv.innerHTML ="Unlock";
+                        UnlockLockDiv.style.float = "right";
+                        UnlockLockDiv.style.marginRight = "25%";
+                        UnlockLockDiv.style.fontSize = "2em";
+                        UnlockLockDiv.style.marginTop = "10%";
+                        UnlockLockDiv.id="Unlock";
+
+        }
+        else{DataDiv.innerHTML="";}
+
+        LockDiv.appendChild(UnlockLockDiv);
+    }
+}
+
+
 
 
 
